42ec8d55ffcaf838ff34e6ba7f168bd8ed9b7998
arvid-persson1 <a.persson04@gmail.com>
2026-02-16 01:15:50 +0100

    Removed unnecessary lint suppressions.

--- src/database.rs
+++ src/database.rs
@@ -7,17 +7,6 @@ mod id;
 pub use id::*;
 
 mod connection;
-#[cfg_attr(
-    not(feature = "server"),
-    expect(
-        unused_imports,
-        reason = "Public items are exported with the server feature."
-    ),
-    expect(
-        unreachable_pub,
-        reason = "Public items are exported with the server feature."
-    )
-)]
 pub use connection::*;
 
 mod auth;
