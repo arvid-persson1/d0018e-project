9a8f33eb0d795d0b41eabdf87f243a626ca8a0f5
Saly <92812037+SalyBoktor@users.noreply.github.com>
2026-03-04 19:27:00 +0100

    fix blank lines

--- src/views/product.rs
+++ src/views/product.rs
@@ -78,6 +78,14 @@ pub fn ProductPage(id: Id<Product>) -> Element {
             
             
             
+            
+            
+            
+            
+            
+            
+            
+            
                             div { class: "flex flex-col items-center",
                                 div { class: "bg-gray-50 rounded-xl p-8 w-full flex justify-center",
                                     if let Some(img) = product.gallery.first() {
