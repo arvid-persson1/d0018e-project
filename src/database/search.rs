e1009579757c72966b75b36ac8d6df8852aa7e7f
arvid-persson1 <a.persson04@gmail.com>
2026-03-10 22:52:23 +0100

    Added dummy import for example code.

--- src/database/search.rs
+++ src/database/search.rs
@@ -72,7 +72,9 @@ pub async fn search_products(query: Box<str>, limit: usize) -> Result<Vec<Search
 }
 
 // TODO: Remove.
-// #[cfg(false)]
+#[cfg(feature = "web")]
+use gloo_timers as _;
+#[cfg(false)]
 mod usage {
     use super::*;
     use dioxus_core::Task;
